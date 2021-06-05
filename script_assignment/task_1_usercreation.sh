467f52b9cc4a42b418d53e6ce312be2345874ad2
student <wintst1@student.op.ac.nz>
2021-06-05 12:49:49 +1200

    coped script into directory

---
+++ script_assignment/task_1_usercreation.sh
@@ -0,0 +1,255 @@
+#!/bin/bash
+
+#checks if the shared folder exists, if not then creates the folder in the /home directory
+#and change permissions to the folder to only be acessed by users of the shared group
+#also checks if a folder has been specified, if not then creates the default shared folder
+#and chekcs what group the user is in before altering permissions.
+createFolder(){
+if [ -z "${folder[$i]}" ]; then
+	echo "no shared folder specified for ${username[$i]}, creating folder: /sharedDefault"
+        sudo mkdir /home/sharedDefault 2> /dev/null
+
+                if [ $? -eq 0 ]; then
+                        echo "folder: /sharedDefault created"
+
+                elif [ -z ${group[$i]} ]; then
+                        sudo chgrp -R default /home/sharedDefault
+                        sudo chmod -R 2775 /home/sharedDefault
+                        sudo ln -s /home/sharedDefault /home/${username[$i]}
+		        if [ $? -eq 0 ]; then
+                		echo "symbolic link created!"
+		        fi
+                else
+                        sudo chgrg -R ${group[$i]} /home/sharedDefault
+                        sudo chmod -R 2775 /home/sharedDefault
+                        sudo ln -s /home/sharedDefault /home/${username[$i]}
+			if [ $? -eq 0 ]; then
+				echo "symbolic link created!"
+			fi
+                fi
+
+elif [ -d "/home${folder[$i]}" ]; then
+        echo "shared folder ${folder[$i]} already exists"
+        sudo chgrp -R ${group[$i]} /home${folder[$i]}
+        sudo chmod -R 2775 /home${folder[$i]}
+        sudo ln -s /home${folder[$i]} /home/${username[$i]}
+        if [ $? -eq 0 ]; then
+                echo "symbolic link created!"
+        fi
+
+else
+        echo "creating shared folder ${folder[$i]}"
+        sudo mkdir /home${folder[$i]}
+        sudo chgrp -R ${group[$i]} /home${folder[$i]}
+        sudo chmod -R 2775 /home${folder[$i]}
+	sudo ln -s /home${folder[$i]} /home/${username[$i]}
+	if [ $? -eq 0 ]; then 
+		echo "symbolic link created!"
+	fi
+fi
+}
+
+
+#checks if the group exists, if not then creates the group, also checks if the group array has an empty
+#string and if it does creates the default group, and assigns the user to that group.
+#if the default group exists it just pipes the error message to /null
+#so that the message doesn't appear in the commmand line
+createGroup() {
+if [ -z "${group[$i]}" ]; then
+	echo "no group specified for ${username[$i]}, assigning to group: default"
+        sudo groupadd default 2> /dev/null
+
+        	if [ $? -eq 0 ]; then
+                	echo "group: default created"
+                	sudo usermod -a -G default ${username[$i]}
+                	echo "added ${username[$i]} to default"
+       	 	else
+                	sudo usermod -a -G default ${username[$i]}
+                	echo "added ${username[$i]} to default"
+        	fi
+
+elif grep -q ${group[$i]} /etc/group; then
+        echo "group ${group[$i]} already exists"
+        sudo usermod -a -G ${group[$i]} ${username[$i]}
+        echo "added ${username[$i]} to ${group[$i]}"
+else
+	echo "creating group ${group[$i]}"
+        sudo groupadd "${group[$i]}"
+        sudo usermod -a -G ${group[$i]} ${username[$i]}
+        echo "added ${username[$i]} to ${group[$i]}"
+fi
+}
+
+#creates the user accounts, sets the default password and changes the password expiry so that
+#the user needs to change their password on their first login.
+createUser() {
+if sudo grep -q ${username[$i]} /etc/passwd; then
+        echo "user ${username[$i]} already exists"
+else
+        sudo useradd -d /home/${username[$i]} -m -s /bin/bash ${username[$i]}
+        echo  ${username[$i]}:${password[$i]} | sudo chpasswd
+	sudo chage -d0 ${username[$i]}
+	echo "user: ${username[$i]} added"
+fi
+}
+
+
+runLoop(){
+for i  in "${!username[@]}"
+do
+	echo "=============================================================================="
+
+        createUser
+        createGroup
+        createFolder
+
+        #if user is in group sudo create the shutdown alias
+        if [[ ${group[$i]} == *"sudo"* ]];then
+                echo "alias SD='sudo shutdown -h now'" >> sudo /home/${username[$i]}/.bashrc
+        fi
+
+        echo "=============================================================================="
+done
+
+}
+
+showDetails(){
+for i in "${!username[@]}"
+        do
+                echo "=============================="
+                echo "Username: ${username[$i]}"
+                echo "Password: ${password[$i]}"
+                echo "Groups: ${group[$i]}"
+                echo "Shared Folder: ${folder[$i]}"
+
+                        if [[ ${group[$i]} == *"sudo"* ]];then
+                                echo "Alias: SD"
+                        fi
+
+                echo "=============================="
+        done
+echo "$usercount users are ready to be created"
+echo "Are you sure you wish to proceed? [Y/N]"
+read response
+
+case "$response" in
+        [yY][eE][sS]|[yY])
+        echo "creating users..."
+        clear
+	runLoop
+        ;;
+        *)
+        echo "Exiting script..."
+        exit 0
+        ;;
+esac
+}
+
+#reads the csv file, asks the user what feild delimeter they wish to use 
+readFile() {
+usercount=0
+echo "Selected $file"
+echo "press enter to continure"
+read; clear
+
+echo "Please specify your CSV feild delimeter: "
+read delimeter extra; clear 
+
+        if [ -n "${extra}" ]; then
+                echo "Error! Please enter only one delimeter!"
+                exit 0
+	elif [ -z "${delimeter// }" ]; then
+                echo "Error! delimeter cannot be empty!"
+        	exit 0
+	elif [[ "${#delimeter}" -gt 1 ]];then
+		echo "Error! feild delimeter can only be 1 character!"
+		exit 0
+	fi
+
+
+
+
+while  IFS="${delimeter}" read -r email birthDate groups sharedFolder
+	do 
+		#creating arrays of groups, usernames, shared folder and passwords
+		#adding first character of name to a string then extracting the 
+		#surname and finally adding the 2 strings together to the username array	
+
+		lastname=$(echo "$email" | sed 's/^[^.]*.//' | sed 's/@.*//')
+		firstchar=${email:0:1}
+		username+=("$firstchar$lastname")
+
+		#extracting the strings needed for the password 
+		month=$(echo "${birthDate:5}" | sed 's/[/].*//')
+		year=$(echo "$birthDate" | sed 's/[/].*//')
+		password+=("$month$year")
+
+		group+=("$groups")
+		folder+=("$sharedFolder")
+
+	let usercount++
+	done < <(tail -n +2 $file)	
+showDetails
+}
+
+
+#asking user for input and general error handling 		
+clear
+if [ $# -eq 0 ]; then
+        echo "Please enter a readable csv file or enter a url: "
+	echo "[1]: Press 1 to enter a csv file"
+	echo "[2]: Press 2 to insert a url" 
+	echo "Press any key to exit"
+	read resp; clear 
+
+	case "$resp" in 
+		[1])
+		echo "Please enter a redable csv file: " 
+                read file extra; clear
+
+                if [ -n "$extra" ]; then
+                        echo "Error! Please insert only one file!"
+                        exit 0
+                elif [ -z "${file// }" ]; then
+                        echo "Error! file required!"
+                        exit 0
+        	fi
+		;;
+		[2])
+		echo "Please enter your url: "
+		read url extra; clear
+		sudo wget -O downloaded_users.csv  $url
+
+		if [ $? -eq 0 ]; then 	
+			file=downloaded_users.csv	
+			echo "File downloaded and renamed to $file"
+                elif [ -n "$extra" ]; then
+                        echo "Error! Please insert only one file!"
+                        exit 0
+                elif [ -z "${url// }" ]; then
+                        echo "Error! url required!"
+                        exit 0
+		else 
+			echo "Error! Something went wrong, exiting..."
+			exit 0
+                fi 
+		;;
+		*)
+		echo "Exiting script..."
+		exit 0
+		;;
+	esac 
+
+elif [ $# -eq 1 ]; then
+	file=$1; clear  
+else 
+	echo "Error! Input can only be 1 file! Exiting.... "
+	exit 0
+fi
+
+if [ ! -f $file ]; then 
+	echo "Error: Cannot find file $file, Exiting...."
+	exit 0
+else
+	readFile $file
+fi
---
+++ script_assignment/task_2_backup.sh
@@ -0,0 +1,179 @@
+#!/bin/bash
+RED='\033[0;31m'
+NC='\033[0m'
+
+#this function handles the scp tranfer command. asks the user for a bunch of informaton like the host address,
+#also validates the input to make sure the user only enters in 1 argument and asks the user to review the 
+#informaiton they provided before moving on. 
+transfer() {
+	echo "File name: $1, Directory to be encrypted: $2"
+	echo "Are you sure you wish to proceed? [Y/N]"
+	read response
+
+	case "$response" in
+        	[yY][eE][sS]|[yY])  
+	        clear
+        	;;
+        	*)
+	        echo "Exiting script..."
+        	exit 0
+	        ;;
+	esac
+
+	echo -n "Please enter the IP address of the remote host: "
+	read ipaddr extra; clear
+
+	if [ -n "$extra" ]; then
+                echo "Error! Please insert only one ip address!"
+                exit 0
+	elif [ -z "${ipaddr// }" ]; then
+                echo "Error! ipaddress required!"
+                exit 0
+        fi
+
+
+	echo -n "Please enter the Port number of the remote host: "
+	read portnum extra; clear 
+
+	if [ -n "$extra" ]; then
+                echo "Error! Please insert only one port number!"
+                exit 0
+	 elif [ -z "${portnum// }" ]; then
+                echo "Error! port number required!"
+                exit 0
+
+        fi
+
+
+	echo -n "Please enter the directory that you wish the file to be stored in on the remote host: "
+	read remotedir extra; clear
+
+        if [ -n "$extra" ]; then
+                echo "Error! Please insert only one directory!"
+                exit 0
+         elif [ -z "${remotedir// }" ]; then
+                echo "Error! directory required!"
+                exit 0
+        fi
+
+
+	echo -n "Please enter the username of the remote host: "
+	read username extra; clear 
+
+        if [ -n "$extra" ]; then
+                echo "Error! Please insert only one username!"
+                exit 0
+         elif [ -z "${username// }" ]; then
+                echo "Error! username required!"
+                exit 0
+        fi
+
+	echo "=============================================================="
+	echo "Confriming details, please read carefully before proceeding..."
+	echo "=============================================================="
+	echo -e "Directory: ${RED}$2${NC}"
+	echo -e	"File: ${RED}$1${NC}"
+	echo -e	"Remote host: ${RED}$ipaddr${NC}"
+	echo -e	"Port num: ${RED}$portnum${NC}"
+	echo -e "Target directory: ${RED}$remotedir${NC}"
+	echo -e	"Host username: ${RED}$username${NC}"
+	
+	echo -e "\nAre you sure you wish to proceed? [Y/N]"
+        read response
+
+        case "$response" in
+                [yY][eE][sS]|[yY])
+                echo "transferring $1 to $username..."
+                ;;
+                *)
+                echo "Exiting script..."
+                exit 0
+                ;;
+        esac
+ 
+	sudo scp -P $portnum $1 $username@$ipaddr:"$remotedir"
+
+	#unsure if these exit status codes are the actual ones that the scp command uses,
+	#at lesat I know that 0 is sucessful, anything else is an error. 
+	if [ $? -eq 0 ]; then 
+		echo "Sucess! File transfer completed"
+		exit 0
+	elif [ $? -eq 74 ]; then
+		echo "Error! Conneciton lost, exiting..."
+		exit 0
+	elif [ $? -eq 79 ]; then 
+		echo "Error! Invalid username, exiting..."
+		exit 0
+	elif [ $? -eq 2 ]; then 
+		echo "Error! Destination is not a directory, exiting..."
+		exit 0 
+	else
+		echo "Erorr encountered! Exiting..."
+	fi
+
+}
+
+
+#the below function compresses the directory into a tarball archive with some basic error checking 
+#in place, then calls the transfer function to run the scp command. 
+tarball() {
+	echo "You've Selected $dirname as Your Directory. Press Enter to Continue..."
+	read; clear
+
+	echo "Preparing Compression..."
+	echo -n "Name your gzip file: "
+	read gzip extra; clear 
+	
+	if [ -n "$extra" ]; then 
+                echo "Error! Please insert only one filename!"
+                exit 0
+	elif [ -z "${gzip// }" ]; then 
+		echo "Error! File cannot have no name!"
+		exit 0
+        fi
+
+	#appened .tar.gz to the filename
+	gzip+=".tar.gz" 
+
+	echo "Beginning compression..." 	
+	tar -cvzf $gzip $dirname   
+	
+	if [ $? -eq 0 ]; then
+		echo "Sucess, directory compressed!"
+		transfer $gzip $dirname
+	else 
+		echo "Error! Something went wrong, exiting..."
+		exit 0
+	fi 
+}
+
+#handles userinput, the user can only enter in one directory, below contains a bunch 
+#of statements to validate the input, eg: checking if the directory exists, making 
+#sure there is only 1 arguemnt entered into the script, if the conditions are met
+#then the function tarball is called. 
+
+if [ $# -eq 0 ]; then 
+	echo -n "Please enter the fullpath to a directory: "
+        read dirname extra 
+
+	if [ -n "$extra" ]; then 
+		echo "Error! Please insert only one directory!"
+		exit 0
+	elif [ -z "${dirname// }" ]; then
+		echo "Error! Directory cannot be nothing!"
+		exit 0 
+	fi 
+
+elif [ $# -eq 1 ]; then
+	dirname=$1 
+else 
+	echo "Error! Input can only take in 1 arugment! Exiting..."
+	exit 0 
+fi  
+
+if [ -d "$dirname" ]; then
+	tarball $dirname
+else
+	echo "Error! Directory does not exist! Exiting..."
+	exit 0
+fi
